d8e89e692b02ef5c8e7d66bfeee1b57634549d8b
Francis Simon <120488335+Simon-Francis@users.noreply.github.com>
2025-10-28 15:28:59 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const axios = require('axios')
 
-const url = 'https://api.github.com/users/'
+const url = 'https://api.github.com/users/Simon-Francis/'
+
+async function getRepos() {
+    let response = await axios.get(url)
+    console.log(responde.data)
+}
+
+getRepos()
